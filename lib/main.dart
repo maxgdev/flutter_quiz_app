dbe8274f511c00b6f5a764ed0b1b5701ea4ff0e0
maxgdev <max@maxgdev.net>
2021-02-23 10:51:41 +0000

    Added Question model and List of Question objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,10 +31,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List questionList = [
+    Question.name("Was Lauryn Hill the first African American woman to win five Grammy Awards in one year?", true),
+    Question.name("Hiram Rhodes Revels was the first African-American to graduate from Harvard University and become a U.S. senator??", true),
+    Question.name("Rapper Tupac Shakur took Ballet and Poetry as student at the Baltimore School of Performing Arts?", true),
+    Question.name("Garrett Morgan invented the three-position traffic signal in 1923 that includes the yellow light?", true),
+    Question.name("Katherine G. Johnson is known for her calculations helping NASA’s space program?", true),
+    Question.name("Cheyney University was the first historically black college in America?", true),
+    Question.name("It took Thomas Jefferson 17 days to write the Declaration of Independence?", true),
+    Question.name("On July 4, 1776, Congress voted to accept the Declaration of Independence?", true),
 
+  ];
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -45,9 +54,14 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-  
             Image.asset('assets/flag.png', width: 250, height: 180),
-
+            Container(
+              height: 120,
+              padding: EdgeInsets.all(15),
+              child: Text("${questionList[0].questionText}",
+              style: TextStyle(fontSize: 16),),
+            ),
+            Spacer(),
           ],
         ),
       ),
