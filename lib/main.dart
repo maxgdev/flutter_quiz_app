c17dbdb4f3193d7be56887d031b9d91bbdcf8ba4
maxgdev <max@maxgdev.net>
2021-02-23 11:21:39 +0000

    Added Raisedbuttons and more styling

--- lib/main.dart
+++ lib/main.dart
@@ -32,15 +32,30 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List questionList = [
-    Question.name("Was Lauryn Hill the first African American woman to win five Grammy Awards in one year?", true),
-    Question.name("Hiram Rhodes Revels was the first African-American to graduate from Harvard University and become a U.S. senator??", true),
-    Question.name("Rapper Tupac Shakur took Ballet and Poetry as student at the Baltimore School of Performing Arts?", true),
-    Question.name("Garrett Morgan invented the three-position traffic signal in 1923 that includes the yellow light?", true),
-    Question.name("Katherine G. Johnson is known for her calculations helping NASA’s space program?", true),
-    Question.name("Cheyney University was the first historically black college in America?", true),
-    Question.name("It took Thomas Jefferson 17 days to write the Declaration of Independence?", true),
-    Question.name("On July 4, 1776, Congress voted to accept the Declaration of Independence?", true),
-
+    Question.name(
+        "Was Lauryn Hill the first African American woman to win five Grammy Awards in one year?",
+        true),
+    Question.name(
+        "Hiram Rhodes Revels was the first African-American to graduate from Harvard University and become a U.S. senator??",
+        true),
+    Question.name(
+        "Rapper Tupac Shakur took Ballet and Poetry as student at the Baltimore School of Performing Arts?",
+        true),
+    Question.name(
+        "Garrett Morgan invented the three-position traffic signal in 1923 that includes the yellow light?",
+        true),
+    Question.name(
+        "Katherine G. Johnson is known for her calculations helping NASA’s space program?",
+        true),
+    Question.name(
+        "Cheyney University was the first historically black college in America?",
+        true),
+    Question.name(
+        "It took Thomas Jefferson 17 days to write the Declaration of Independence?",
+        true),
+    Question.name(
+        "On July 4, 1776, Congress voted to accept the Declaration of Independence?",
+        true),
   ];
   @override
   Widget build(BuildContext context) {
@@ -56,15 +71,32 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Image.asset('assets/flag.png', width: 250, height: 180),
             Container(
+              margin: EdgeInsets.all(15),
               height: 120,
               padding: EdgeInsets.all(15),
-              child: Text("${questionList[0].questionText}",
-              style: TextStyle(fontSize: 16),),
+              decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                      color: Colors.white, style: BorderStyle.solid)),
+              child: Text(
+                "${questionList[0].questionText}",
+                style: TextStyle(fontSize: 16),
+              ),
             ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              RaisedButton(onPressed: () => _checkAnswer(), child: Text("True"),color: Colors.blue.shade200,),
+              RaisedButton(onPressed: () => _checkAnswer(), child: Text("False"),color: Colors.blue.shade200),
+              RaisedButton(onPressed: () => _checkAnswer(), child: Text("Next"),color: Colors.blue.shade200),
+            ]),
             Spacer(),
           ],
         ),
       ),
     );
   }
+}
+
+_checkAnswer() {
+  print('button pressed');
 }
