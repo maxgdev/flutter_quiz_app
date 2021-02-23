e1858846a145aa768734918a5ab2ab96e322ff1b
maxgdev <max@maxgdev.net>
2021-02-23 09:47:42 +0000

    Cleanup after 'flutter create flutter_quiz_app'

---
+++ README.md
@@ -0,0 +1,9 @@
+# flutter_quiz_app
+
+Flutter Quiz app
+
+## Creedits
+- [Packt] 
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- [online documentation](https://flutter.dev/docs), which offers tutorials,
+
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Hide the Debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Quiz App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Quiz App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Flutter Quiz App',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
